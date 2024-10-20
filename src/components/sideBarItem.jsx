8feef8b79c5d89b9f512d6e0a320845f1d759f13
Maid Mešić <maidmesic0503@gmail.com>
2024-10-20 18:44:33 +0200

    Update sideBarItem.jsx

--- src/components/sideBarItem.jsx
+++ src/components/sideBarItem.jsx
@@ -7,11 +7,11 @@ export default function sideBarItem({name,to}) {
             <NavLink to={to}>
                 {({isActive}) => (
                     isActive ? ( <img
-                        src={`/src/assets/active/${name}-active.png`}
+                        src={`/assets/active/${name}-active.png`}
                         alt={name}
                         className={`${styles.active} ${styles.icon}`}
                     />) : (<img
-                        src={`/src/assets/default/${name}.png`}
+                        src={`/assets/default/${name}.png`}
                         alt={name}
                         className={styles.icon}
                     />)
